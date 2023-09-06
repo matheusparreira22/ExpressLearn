faf1b33d58fd38c2007aecce508e6be3495097b9
matheusparreira22 <matheusparreira972@gmail.com>
2023-09-06 10:07:10 -0300

    'response-json'

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res)=>{
     res.send ('<h1>Minha lista de tarefas</h1>');
 })
 
+app.get('/json', (req, res)=>{
+    res.json({title:'task exemple', done:true})
+})
+
 app.listen(3000, ()=>{
     console.log('servidor foi iniciado')
 })
